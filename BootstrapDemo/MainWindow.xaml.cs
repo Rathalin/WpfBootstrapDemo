311055c77861b50c429d92a76147404479ffe304
Rhatalin <danielflo1997@hotmail.com>
2019-03-31 19:37:23 +0200

    Add first version of demo

---
+++ BootstrapDemo/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BootstrapDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TBl_BtnsExample.Text = "<Button Content=\"Primary\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Secondary\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Success\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Danger\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Warning\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Info\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Light\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Dark\" Style=\"{StaticResource btn-primary}\"></Button>" + "\n";
+            TBl_OutlBtnsExample.Text = "<Button Content=\"Primary\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Secondary\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Success\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Danger\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Warning\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Info\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Light\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n" +
+                "<Button Content=\"Dark\" Style=\"{StaticResource btn-outline-primary}\"></Button>" + "\n";
+        }
+    }
+}
